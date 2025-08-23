6307f281f53d11dca29fbe21b339568d7d03143f
pythonpydev <pythonpydev1@gmail.com>
2025-08-23 09:46:00 +0100

    updated wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,13 @@
-from total_bankroll import create_app
+import sys
+import os
 
-app = create_app()
+# Add project directory to Python path
+project_home = '/home/pythonpydev/total_bankroll/src'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+# Set Flask environment to production
+os.environ['FLASK_ENV'] = 'production'
+
+# Import the Flask application
+from total_bankroll.app import app as application
