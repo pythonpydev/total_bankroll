7f44fde31f5a11adf44d0074732465d0ffa762e0
pythonpydev <pythonpydev1@gmail.com>
2025-10-26 18:35:14 +0000

    updated extensions.py

--- src/total_bankroll/extensions.py
+++ src/total_bankroll/extensions.py
@@ -1,7 +1,10 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_mail import Mail
-from flask_wtf.csrf import CSRFProtect
+from flask_bcrypt import Bcrypt
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+from flask_mailman import Mail
+from flask_principal import Principal
 
-db = SQLAlchemy()
+bcrypt = Bcrypt()
+limiter = Limiter(key_func=get_remote_address)
 mail = Mail()
-csrf = CSRFProtect()
+principal = Principal()
